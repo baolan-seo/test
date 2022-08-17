a3739974b0f58401f4dbca4fc52f1b9204016913
sbr <sbr@SBR-PC.iwi.co.kr>
2022-08-17 16:06:45 +0900

    커밋

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,16 +1,21 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class TestController {
 
+public class TestController {
+	
+	@Value("${test.config}")
+	String config;
+	
 	
 	@RequestMapping("/test")
 	public String test() {
-		
+		System.out.print("================== : " + config);
 		
 		return "test~";
 	}
